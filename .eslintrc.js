168d519187e402cc3381927cb8f8c1e5e5ea6395
parkyejin <uiop5809@naver.com>
2023-07-07 01:33:22 +0900

    Chore: ESLint + prettier 추가

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  globals: {
+    module: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error'
+  }
+};
