ae47e6f2566426b8a9802f8bfbb89f0674606190
parkyejin <uiop5809@naver.com>
2023-07-07 16:57:14 +0900

    Design: 공통 color 설정 (#1)

---
+++ src/styles/color.ts
@@ -0,0 +1,17 @@
+const COLOR = {
+  MAIN: "#DACAFF",
+  MAIN2: "#AD71EA",
+  MAIN3: "#FAF8FF",
+  MAIN4: "#F4EFFF",
+  MAINDARK: "#222222",
+
+  ALARM: "#FF5C5C",
+
+  GRAY1: "#4E4F4E",
+  GRAY2: "#7A7A7B",
+  GRAY3: "#A7A7A7",
+  GRAY4: "#D4D3D3",
+  GRAY5: "#F5F5F5",
+} as const;
+
+export default COLOR;
