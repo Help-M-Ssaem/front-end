451a57961f690893c2071050a9aad00338b2b502
parkyejin <uiop5809@naver.com>
2023-07-09 03:16:04 +0900

    Design: header 컴포넌트 관련 icon 추가 (#6)

---
+++ src/constants/CommonIcons.tsx
@@ -0,0 +1,24 @@
+export const SearchIcon = () => {
+  return (
+    <svg
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 22C18.4183 22 22 18.4183 22 14C22 9.58172 18.4183 6 14 6C9.58172 6 6 9.58172 6 14C6 18.4183 9.58172 22 14 22Z"
+        stroke="#222222"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M27 27L20 20"
+        stroke="#222222"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
