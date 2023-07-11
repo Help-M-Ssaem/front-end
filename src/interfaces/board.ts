e2ef6c656df8a9b19eae15894dae4505af1fc615
parkyejin <uiop5809@naver.com>
2023-07-11 20:43:30 +0900

    Feat: board, navbar interface 정의 (#8)

---
+++ src/interfaces/board.ts
@@ -0,0 +1,13 @@
+export interface Board {
+  id: number;
+  name: string;
+  profile: string;
+  thumbnail: string;
+  mbti: string;
+  badge: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  like: number;
+  comment: number;
+}
