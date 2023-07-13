0a222ee42268daf888f9a05eca78aed6c700e9b5
parkyejin <uiop5809@naver.com>
2023-07-14 01:42:40 +0900

    Feat: 게시판 생성 editor 라이브러리 (#8)

--- src/pages/board/create.tsx
+++ src/pages/board/create.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { css } from "@emotion/react";
+import COLOR from "../../styles/color";
 
 const CreateBoardPage = () => {
-  return <div>CreateBoardPage</div>;
+  return (
+    <div css={EditorContainer}>
+      <Editor
+        initialValue="hello react editor world!"
+        previewStyle="vertical"
+        height="30rem"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+      />
+    </div>
+  );
 };
 
 export default CreateBoardPage;
+
+const EditorContainer = css`
+  width: calc(100% + 30rem);
+  margin-left: -15rem;
+  background: ${COLOR.MAIN3};
+  padding: 1.5rem 15rem;
+`;
