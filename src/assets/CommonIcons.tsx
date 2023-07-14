d56f63d2d3d9d6496ff9fb50b7c5756483c2cb49
parkyejin <uiop5809@naver.com>
2023-07-14 19:34:06 +0900

    Fix: svg console 오류 해결

--- src/assets/CommonIcons.tsx
+++ src/assets/CommonIcons.tsx
@@ -10,14 +10,14 @@ export const SearchIcon = () => {
       <path
         d="M14 22C18.4183 22 22 18.4183 22 14C22 9.58172 18.4183 6 14 6C9.58172 6 6 9.58172 6 14C6 18.4183 9.58172 22 14 22Z"
         stroke="#222222"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M27 27L20 20"
         stroke="#222222"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
