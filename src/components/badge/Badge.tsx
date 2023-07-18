80f588f98414da1c9cb4787a6de5ba06c4033164
parkyejin <uiop5809@naver.com>
2023-07-18 16:29:00 +0900

    Design: Matching 페이지 디자인 (#22)

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -17,7 +17,7 @@ const Badge = ({ mbti, color }: BadgeProps) => {
     color: ${COLOR.WHITE};
     font-size: ${FONT.SIZE.BODY};
     font-weight: ${FONT.WEIGHT.REGULAR};
-    background: ${color || "transparent"};
+    background: ${color || "#F8CAFF"};
     padding: 0.1rem 0.5rem;
     border-radius: 0.9rem;
     margin-right: 0.5rem;
---
+++ src/interfaces/matching.ts
@@ -0,0 +1,11 @@
+export interface Matching {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  thumbnail?: string;
+  mbti1: string;
+  mbti2: string;
+  color1?: string;
+  color2?: string;
+}
