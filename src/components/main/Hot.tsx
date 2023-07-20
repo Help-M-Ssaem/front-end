9e72b746318bef241914a885411f061b3cf937a5
parkyejin <uiop5809@naver.com>
2023-07-21 00:16:44 +0900

    Design: main 페이지 디자인 (#28)

--- src/components/main/Hot.tsx
+++ src/components/main/Hot.tsx
@@ -1,7 +1,49 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import Container from "../container/Container";
+import COLOR from "../../styles/color";
+import FONT from "../../styles/font";
 
-const Hot = () => {
-  return <Container></Container>;
+interface hotProps {
+  board: any;
+}
+
+const Hot = ({ board }: hotProps) => {
+  return (
+    <Container
+      background={COLOR.WHITE}
+      style={{
+        marginRight: "1rem",
+      }}
+    >
+      <div css={categoryCSS}>{board.category}</div>
+      <div css={titleCSS}>{board.title}</div>
+      <div css={textCSS}>바로가기</div>
+    </Container>
+  );
 };
 
 export default Hot;
+
+const categoryCSS = css`
+  color: ${COLOR.GRAY2};
+  font-size: ${FONT.SIZE.HEADLINE};
+  font-weight: ${FONT.WEIGHT.SEMIBOLD};
+  margin-bottom: 0.5rem;
+`;
+
+const titleCSS = css`
+  font-size: ${FONT.SIZE.TITLE3};
+  font-weight: ${FONT.WEIGHT.BOLD};
+  margin-bottom: 0.5rem;
+  line-height: 1.4rem;
+`;
+
+const textCSS = css`
+  color: ${COLOR.GRAY2};
+  font-size: ${FONT.SIZE.FOOTNOTE};
+  font-weight: ${FONT.WEIGHT.REGULAR};
+  text-decoration: underline;
+  text-align: right;
+  cursor: pointer;
+`;
