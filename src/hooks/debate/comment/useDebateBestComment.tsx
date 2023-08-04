2efb18fd4727d6c23eb0afec2955ffae05739232
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2023-08-04 22:44:50 +0900

    Design: 토론글 댓글 로직 생성 중 아직 미완

---
+++ src/hooks/debate/comment/useDebateBestComment.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from "react-query";
+import { mssaemAxios as axios } from "../../../apis/axios";
+import { commentKeys } from "../../../constants/commentKey";
+
+async function getDebateBestComment(id: number): Promise<Comment[]> {
+  const { data } = await axios.get(`/discussions/${id}/comments/best`);
+  return data;
+}
+
+interface UseDebateComment {
+  bestComments?: Comment[];
+}
+
+export function useDebateBestComment(id: number): UseDebateComment {
+  const { data: bestComments } = useQuery(commentKeys.best, () =>
+    getDebateBestComment(id),
+  );
+  return { bestComments };
+}
