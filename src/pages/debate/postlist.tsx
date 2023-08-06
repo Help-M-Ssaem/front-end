3d7520ffe1b0a703af86916f07416fc61b33d0ef
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2023-08-06 19:40:54 +0900

    Docs: Hot, 일반 토론글 페이지네이션 적용

--- src/pages/debate/postlist.tsx
+++ src/pages/debate/postlist.tsx
@@ -1,56 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import DebateComponent from "../../components/debate/debate";
-import { css } from "@emotion/react";
-import { useNavigate } from "react-router";
-import FONT from "../../styles/font";
-import Container from "../../components/container/Container";
-import { Debate } from "../../interfaces/debate";
-import { useDebateList } from "../../hooks/debate/useDebateList";
-import COLOR from "../../styles/color";
-
+import Text from "../../components/text/Text";
+import PageDebate from "../../components/debate/pageMapingDebate/PageDebate";
 const PostListDebatePage = () => {
-  const navigate = useNavigate();
-  const { debateList } = useDebateList(0, 6);
   return (
-    <Container addCSS={containerCSS}>
-      <div css={headerCSS}>
-        <div css={titleBoxCSS}>MBTI 과몰입 토론</div>
-      </div>
-
-      <div>
-        {debateList &&
-          debateList.result.map((debate: Debate, index) => (
-            <DebateComponent
-              debate={debate}
-              key={debate.id}
-              onClick={() => navigate(`/debate/${debate.id}`)}
-              index={index}
-            />
-          ))}
-      </div>
-    </Container>
+  <>
+    <Text>MBTI 과몰입 토론</Text>
+    <PageDebate
+      pathMov = {"discusstion"}
+      />
+    </>
   );
 };
 
 export default PostListDebatePage;
-
-const containerCSS = css`
-  background: ${COLOR.WHITE};
-`;
-
-const headerCSS = css`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: calc(100% + 30rem);
-  margin-left: -15rem;
-  padding: 0 15rem;
-  margin-bottom: 1rem;
-`;
-
-const titleBoxCSS = css`
-  align-items: center;
-  margin: 1rem 0;
-  font-size: ${FONT.SIZE.TITLE3};
-  font-weight: ${FONT.WEIGHT.BOLD};
-`;
