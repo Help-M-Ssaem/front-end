495237ce90da3a5bfd2869066f48363ed94588ac
parkyejin <uiop5809@naver.com>
2023-08-12 01:18:21 +0900

    Design: object-fit cover 추가

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -92,6 +92,8 @@ const marginRightCSS = css`
 const thumbnailCSS = css`
   width: 9rem;
   height: 9rem;
+  object-fit: cover;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const detailCSS = css`
