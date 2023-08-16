378d9c04e9544583d6504ee989f04f62c55a7abc
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2023-08-16 16:35:53 +0900

    Fix: 충돌 오류

--- src/components/debate/debate.tsx
+++ src/components/debate/debate.tsx
@@ -9,8 +9,7 @@ import RedButton from "../button/plusbutton/RedButton";
 import VoteItemList from "./vote/VoteItemList";
 import { useNavigate } from "react-router";
 import Text from "../text/Text";
-import { useSelectedItem } from "../../hooks/debate/useVoteItem";
-
+import { useState } from "react";
 interface DebateProps {
   debate: Debate;
   onClick: (id: number) => void;
@@ -20,24 +19,31 @@ interface DebateProps {
 
 const DebateComponent = ({ debate, onClick, mode, index }: DebateProps) => {
   const navigate = useNavigate();
-  const optionSelected = debate.options.some((option) => option.selected === true);
-  // const selectedMutation = useSelectedItem(debate.id, id);
-  // const handleClick = () => {
-  //     selectedMutation.mutate();
-  // }
+  const isNotSearchResult = !window.location.href.includes("search");
+  const [isNotSearch, setIsNotSearch] = useState(isNotSearchResult);
+
+  const debateBoxCSS = css`
+    padding: ${isNotSearch && " 1.5rem"};
+    margin: ${isNotSearch && "0 0 4rem 0"};
+    background: ${COLOR.MAIN3};
+    border-radius: 1.4rem;
+    position: relative;
+  `;
+
   return (
-    <div 
-      css={[debateBoxCSS]} 
-      >
-      
-        {index % 6 === 0 && (
-          <div css={buttonBoxCSS}>
-          {mode === "discusstion" ? <Text>MBTI 과몰입 토론</Text> : <Text>HOT 토론글</Text>}
+    <div css={[debateBoxCSS, onclickCSS]} onClick={() => onClick(debate.id)}>
+      {index % 6 === 0 && isNotSearch && (
+        <div css={buttonBoxCSS}>
+          {mode === "discusstion" ? (
+            <Text>MBTI 과몰입 토론</Text>
+          ) : (
+            <Text>HOT 토론글</Text>
+          )}
           <Button onClick={() => navigate("/debate/create")}>글 쓰기</Button>
-          </div>
-        )}
-      <div>
-      <div css={[leftCSS, onclickCSS]} onClick={() => onClick(debate.id)}>
+        </div>
+      )}
+
+      <div css={leftCSS}>
         <div css={dateTop}>
           <div css={profileBoxCSS}>
             <Profile
@@ -49,18 +55,16 @@ const DebateComponent = ({ debate, onClick, mode, index }: DebateProps) => {
           </div>
           <div css={marginRightCSS}>{debate.createdAt}</div>
         </div>
-          <div css={titleCSS}>{debate.title}</div>
-          <div css={contentCSS}>{debate.content}</div>
-        </div>
-        <VoteItemList options={debate.options} debateId={debate.id} />
+        <div css={titleCSS}>{debate.title}</div>
+        <div css={contentCSS}>{debate.content}</div>
+      </div>
+      <VoteItemList options={debate.options} debateId={debate.id} />
 
-        <div css={[detailCSS, onclickCSS]} onClick={() => onClick(debate.id)}>
-          <RedButton
-            count={`${debate.participantCount}명이 참여중`}
-          ></RedButton>
-          <div>댓글 {debate.commentCount}</div>
-        </div>
-        </div>
+      <div css={detailCSS}>
+        <RedButton count={`${debate.participantCount}명이 참여중`}></RedButton>
+        <div>댓글 {debate.commentCount}</div>
+      </div>
+
       <div css={bottomLineCSS}>&nbsp;</div>
     </div>
   );
@@ -87,14 +91,14 @@ const bottomLineCSS = css`
 const leftCSS = css`
   display: flex;
   flex-direction: column;
-  flex-grow: 1; 
+  flex-grow: 1;
   padding-top: 1.4rem;
 `;
 
 const dateTop = css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const titleCSS = css`
@@ -141,13 +145,13 @@ const buttonBoxCSS = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top:1.5rem;
+  padding-top: 1.5rem;
   border-bottom: 1px solid ${COLOR.MAIN};
   padding-bottom: 1.4rem;
 `;
 
 const onclickCSS = css`
-cursor: pointer;
+  cursor: pointer;
 `;
 
 //=---------------------------------------------
