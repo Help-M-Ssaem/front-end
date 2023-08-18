1c099ee4da81f5fa16699a5e74386270c92d833a
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2023-08-18 22:42:23 +0900

    Fix: 알람페이지에서 refetch 반영

--- src/hooks/alarm/usePageAlarm.tsx
+++ src/hooks/alarm/usePageAlarm.tsx
@@ -1,20 +1,37 @@
 import { mssaemAxios as axios } from "../../apis/axios";
 import { AlarmList } from "../../interfaces/alarm";
 import { useQuery } from "react-query";
-import { alarmKeys } from "../../constants/alarmKey";
-export async function getDebateDetail(page: number): Promise<AlarmList> {
-    const { data } = await axios.get(`/member/notifications?page=${page}&size=${10}`);
-    return data;
-  }
-interface UseAlarmList {
-    alarmList? : AlarmList;
-}
-export function useAlarmPaging ( page: number) : UseAlarmList {
-    const {data:alarmList} = useQuery("alramList", ()=>
-    getDebateDetail(page),
-    )
-    return{alarmList}
-  };
+
+export function useAlarmPaging ( page: number) {
+  const alarmpageKey = "alramList";
+  const { data : alarmList, refetch } = useQuery<AlarmList> (
+    alarmpageKey,
+    async () =>  {
+      const { data } = await axios.get(
+        `/member/notifications?page=${page}&size=${10}`
+      );
+      return data;
+    }
+  );
+  return {alarmList, refetch}
+};
   
-  export default useAlarmPaging;
+  // export function useDebatePaging(path: string, page: number, postId: number) {
+  //   const queryKey = path === 'discusstion' ? debateKeys.all : debateKeys.hot;
+  //   const { data: debateList, refetch } = useQuery<DebateList>(
+  //     queryKey,
+  //     async () => {
+  //       const { data } = await axios.get(
+  //         `/discussions${path === 'discusstion' ? '' : '/hot'}?discussionId=${postId}&page=${page}&size=6`
+  //       );
+  //       return data;
+  //     }
+  //   );
+  
+  //   return { debateList, refetch };
+  // }
+  
+
+
+
   
