db62d1137ad0218661dcfcfe29071bed27a17c3e
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2023-08-18 20:08:25 +0900

    Fix: 토론글 페이지네이션 수정, 투표시 바로 반영

--- src/hooks/debate/useDebatePaging.tsx
+++ src/hooks/debate/useDebatePaging.tsx
@@ -1,28 +1,19 @@
-import { useEffect, useState } from "react";
-import { mssaemAxios as axios } from "../../apis/axios";
-import { DebateList } from "../../interfaces/debate";
-import { useQueryClient } from "react-query";
-import { debateKeys } from "../../constants/debateKey";
+import { useQuery } from 'react-query';
+import { mssaemAxios as axios } from '../../apis/axios';
+import { DebateList } from '../../interfaces/debate';
+import { debateKeys } from '../../constants/debateKey';
 
-export function useDebatePaging ( path: string, page: number, postId: number) {
-    const [DebateList, setDebateList] = useState<DebateList>();
-    const queryClient = useQueryClient();
-  
-    useEffect(() => {
-      if (path === "discusstion") {
-        axios.get(`/discussions?discussionId=${postId}&page=${page}&size=${6}`).then((res) => {
-            setDebateList(res.data);
-            queryClient.setQueryData("debate", res.data);
-        });
-    } else if (path === "hotDiscusstion"){
-        axios.get(`/discussions/hot?discussionId=${postId}&page=${page}&size=${6}`).then((res) => {
-            setDebateList(res.data);
-            queryClient.setQueryData("debatehot", res.data);
-        }); 
+export function useDebatePaging(path: string, page: number, postId: number) {
+  const queryKey = path === 'discusstion' ? debateKeys.all : debateKeys.hot;
+  const { data: debateList, refetch } = useQuery<DebateList>(
+    queryKey,
+    async () => {
+      const { data } = await axios.get(
+        `/discussions${path === 'discusstion' ? '' : '/hot'}?discussionId=${postId}&page=${page}&size=6`
+      );
+      return data;
     }
-}, [path, page, queryClient, postId ]);
-    return DebateList;
-  };
-  
-  export default useDebatePaging;
-  
+  );
+
+  return { debateList, refetch };
+}
