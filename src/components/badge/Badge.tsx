fe2f0528aee617ea6f35c3716891c2e7e448d44f
parkyejin <uiop5809@naver.com>
2023-08-21 18:42:33 +0900

    Design: badge color 추가

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 import COLOR from "../../styles/color";
 import FONT from "../../styles/font";
 import { MBTICOLOR } from "../../styles/color";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface BadgeProps {
   mbti: string;
@@ -11,18 +11,26 @@ interface BadgeProps {
 type MBTIColors = typeof MBTICOLOR;
 
 const Badge = ({ mbti }: BadgeProps) => {
-  const badgeMbti = mbti.toUpperCase();
   const [color, setColor] = useState("#7A7A7B");
+  const [changeColor, setChangeColor] = useState(false);
 
-  // if (mbti === "엠비티어른" || mbti === "FUNFUN") {
-  //   setColor("#C85287");
-  // } else if (mbti === "MBTMI") {
-  //   setColor("#F6E0A6");
-  // } else if (mbti === "엠비티라노") {
-  //   setColor("#00AF76");
-  // } else if (mbti === "NEWBIE") {
-  //   setColor("#9BF266");
-  // }
+  useEffect(() => {
+    if (mbti === "엠비티어른" || mbti === "FUNFUN") {
+      setColor("#C85287");
+      setChangeColor(true);
+    } else if (mbti === "MBTMI") {
+      setColor("#F6E0A6");
+      setChangeColor(true);
+    } else if (mbti === "엠비티라노") {
+      setColor("#00AF76");
+      setChangeColor(true);
+    } else if (mbti === "NEWBIE") {
+      setColor("#9BF266");
+      setChangeColor(true);
+    }
+  }, []);
+
+  const formattedMbti = mbti && mbti.length === 4 && mbti.toUpperCase();
 
   const badgeCSS = css`
     display: flex;
@@ -32,7 +40,9 @@ const Badge = ({ mbti }: BadgeProps) => {
     color: ${COLOR.WHITE};
     font-size: ${FONT.SIZE.BODY};
     font-weight: ${FONT.WEIGHT.REGULAR};
-    background: ${MBTICOLOR[badgeMbti as keyof MBTIColors] || color};
+    background: ${changeColor
+      ? color
+      : MBTICOLOR[formattedMbti as keyof MBTIColors]};
     padding: 0.15rem 0.5rem;
     border-radius: 0.9rem;
     margin-right: 0.4rem;
